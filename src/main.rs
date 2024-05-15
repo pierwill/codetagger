d151d873949da5320b199f89607f1d94c285d3dc
pierwill <pierwill@users.noreply.github.com>
2024-05-15 10:42:29 -0500

    Add basic language finding function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
-use std::collections::BTreeMap;
+#![allow(unused_variables, dead_code)]
+
+use std::collections::{BTreeMap, HashSet};
 use std::fs::read_to_string;
 
 use ansi_term::Colour::White;
-use clap::ArgAction;
-use clap::Parser;
+use clap::{ArgAction, Parser};
 use regex::Regex;
 use walkdir::WalkDir;
 
-const CODE_TABS_STRINGS_1: &str = "tabs-selector:: drivers";
-const CODE_TABS_STRINGS_2: &str = "tabs-drivers::";
+#[derive(Debug)]
+pub(crate) enum Reason {
+    CodeExample(String),
+    Languages(HashSet<Language>),
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub(crate) enum Language {
+    Go,
+    Csharp,
+    Python,
+    C,
+    Php,
+    Ruby,
+    Cpp,
+    JavaSync,
+    JavaAsync,
+    Scala,
+    Nodejs,
+    Kotlin,
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
+pub(crate) struct Args {
     /// In order to make changes to the files,
     /// run `with --dryrun=false`.
     #[clap(long, short,
@@ -27,21 +47,24 @@ struct Args {
     verbose: bool,
 }
 
+const CODE_TABS_STRINGS_1: &str = "tabs-selector:: drivers";
+const CODE_TABS_STRINGS_2: &str = "tabs-drivers::";
+
 fn main() {
     let args = Args::parse();
     let _debug = false;
     let dryrun = args.dryrun;
     let repo = args.repo;
 
-    let mut files_needing_tag_and_reason: BTreeMap<String, Option<String>> = BTreeMap::default();
-    let mut stringlist: Vec<String> = vec![];
+    let mut files_needing_tag_and_reason: BTreeMap<String, Option<Reason>> = BTreeMap::default();
+    let mut match_string_list: Vec<String> = vec![];
     let mut includes_with_code_tabs: Vec<String> = get_includes_with_code_tabs(repo.clone());
-    stringlist.append(&mut includes_with_code_tabs);
-    stringlist.push(CODE_TABS_STRINGS_1.to_string());
-    stringlist.push(CODE_TABS_STRINGS_2.to_string());
+    match_string_list.append(&mut includes_with_code_tabs);
+    match_string_list.push(CODE_TABS_STRINGS_1.to_string());
+    match_string_list.push(CODE_TABS_STRINGS_2.to_string());
 
     if args.verbose {
-        println!("Strings to look for: {:#?}", stringlist);
+        println!("Strings to look for: {:#?}", match_string_list);
     }
 
     // Loop through all sub directories looking
@@ -55,12 +78,16 @@ fn main() {
         }
         let filepath = String::from(entry_path.to_string_lossy());
 
-        let (needs_tag, reason) = check_needs_tag(&filepath, stringlist.clone());
-
+        let (needs_tag, reason) =
+            check_needs_code_example_tag(&filepath, match_string_list.clone());
         if needs_tag {
             files_needing_tag_and_reason.insert(filepath.clone(), reason);
         }
-        // use indexmap?
+
+        let (needs_tag, reason) = check_needs_lang_metadata(&filepath);
+        if needs_tag {
+            files_needing_tag_and_reason.insert(filepath.clone(), reason);
+        }
     }
 
     if args.verbose {
@@ -73,13 +100,16 @@ fn main() {
 
     // For all files needing tagging,
     // add `code example` to meta keywords
-    println!("📝 Tagging...");
+    println!("📝 Tagging for \"code example\" ...");
     for (file, _) in files_needing_tag_and_reason {
         let meta_keywords: Option<String> = get_meta_keywords(&file);
         let has_meta_keywords: bool = meta_keywords.is_some();
 
         if has_meta_keywords && meta_keywords.unwrap().contains("code example") {
             // File has already has `code example` in meta keywords
+            if args.verbose {
+                println!("💁 {file} already has code-example tag");
+            }
             continue;
         } else {
             add_to_meta_keywords(&file, dryrun)
@@ -127,23 +157,55 @@ fn get_includes_with_code_tabs(repo: String) -> Vec<String> {
     includes_with_code_tabs
 }
 
-// Returns true if it needs a tag, and the string value that matched.
-// This string is the reason we added the tag.
-fn check_needs_tag(path: &str, strings: Vec<String>) -> (bool, Option<String>) {
+// Returns true if the file needs a "code example" tag, and the Reason.
+fn check_needs_code_example_tag(path: &str, strings: Vec<String>) -> (bool, Option<Reason>) {
     if path.contains("/includes/") {
         return (false, None);
     }
 
     let lines = read_lines(path);
 
-    for line in lines {
+    for line in &lines {
         for item in &strings {
             if line.contains(item) {
-                return (true, Some(item.to_string()));
+                return (true, Some(Reason::CodeExample(item.to_string())));
             }
         }
     }
+
     (false, None)
+}
+
+// Returns true if the file needs a "code example" tag, and the Reason.
+fn check_needs_lang_metadata(path: &str) -> (bool, Option<Reason>) {
+    let lines = read_lines(path);
+
+    let mut langs: HashSet<Language> = HashSet::new();
+
+    for line in lines.iter() {
+        if line.contains(CODE_TABS_STRINGS_2) {
+            let tabids = get_tabids(&lines);
+            // println!("{:?}", tabids);
+            if tabids.contains(&String::from("go")) {
+                langs.insert(Language::Go);
+            }
+            if tabids.contains(&String::from("python")) {
+                langs.insert(Language::Python);
+            }
+            if tabids.contains(&String::from("csharp")) {
+                langs.insert(Language::Csharp);
+            }
+            if tabids.contains(&String::from("cpp")) {
+                langs.insert(Language::Csharp);
+            }
+        }
+    }
+
+    if langs.is_empty() {
+        return (false, None);
+    } else {
+        return (true, Some(Reason::Languages(langs)));
+    }
 }
 
 fn get_meta_keywords(path: &str) -> Option<String> {
@@ -154,6 +216,16 @@ fn get_meta_keywords(path: &str) -> Option<String> {
         }
     }
     None
+}
+
+fn get_tabids(lines: &Vec<String>) -> Vec<String> {
+    let mut tabids: Vec<String> = vec![];
+    for line in lines.iter() {
+        if line.contains(":tabid:") {
+            tabids.push(line.split(":tabid:").map(|s| s.trim()).collect::<Vec<_>>()[1].to_string());
+        }
+    }
+    tabids
 }
 
 fn add_to_meta_keywords(path: &str, dryrun: bool) {
